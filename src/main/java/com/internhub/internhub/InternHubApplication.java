e092b4d95f2c09e5f97e73fe600439f59df87c74
Robitanu <robeeert.postu@gmail.com>
2026-02-26 19:49:54 +0200

    chore: bootstrap project (docker postgres + flyway v1 + config)

---
+++ src/main/java/com/internhub/internhub/InternHubApplication.java
@@ -0,0 +1,13 @@
+package com.internhub.internhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/internhub/internhub/InternHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internhub.internhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
